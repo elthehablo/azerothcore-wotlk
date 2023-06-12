66153a017a65ec958bc65b20aeeba1c7387b2a39
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-06-12 10:07:28 +0200

    initial commit
    change Maghteridon to mostly only use scheduler instead of UpdateAI

--- src/server/scripts/Outland/HellfireCitadel/MagtheridonsLair/boss_magtheridon.cpp
+++ src/server/scripts/Outland/HellfireCitadel/MagtheridonsLair/boss_magtheridon.cpp
@@ -57,23 +57,6 @@ enum Spells
 
 enum Events
 {
-    EVENT_EMOTE1                = 1,
-    EVENT_EMOTE2                = 2,
-    EVENT_EMOTE3                = 3,
-    EVENT_ENTER_COMBAT          = 4,
-    EVENT_RECENTLY_SPOKEN       = 5,
-
-    EVENT_CLEAVE                = 10,
-    EVENT_BLAST_NOVA            = 11,
-    EVENT_BLAZE                 = 12,
-    EVENT_ENRAGE                = 13,
-    EVENT_QUAKE                 = 14,
-    EVENT_CHECK_HEALTH          = 15,
-    EVENT_COLLAPSE_CEIL         = 16,
-    EVENT_COLLAPSE_DAMAGE       = 17,
-    EVENT_DEBRIS                = 18,
-
-    EVENT_RANDOM_TAUNT          = 30,
     EVENT_CHECK_GRASP           = 31,
     EVENT_CANCEL_GRASP_CHECK    = 32
 };
@@ -102,19 +85,56 @@ public:
 
     struct boss_magtheridonAI : public BossAI
     {
-        boss_magtheridonAI(Creature* creature) : BossAI(creature, TYPE_MAGTHERIDON) { }
+        boss_magtheridonAI(Creature* creature) : BossAI(creature, TYPE_MAGTHERIDON) 
+        {
+            scheduler.SetValidator([this]
+            {
+                return !me->HasUnitState(UNIT_STATE_CASTING);
+            });
+        }
 
         EventMap events2;
+
+
 
         void Reset() override
         {
             events2.Reset();
-            events2.ScheduleEvent(EVENT_RANDOM_TAUNT, 90000);
+            scheduler.Schedule(90s, [this](TaskContext context)
+            {
+                Talk(SAY_TAUNT);
+                context.Repeat(90s);
+            });
             _Reset();
             me->CastSpell(me, SPELL_SHADOW_CAGE, true);
             me->SetReactState(REACT_PASSIVE);
             me->SetUnitFlag(UNIT_FLAG_NOT_SELECTABLE);
             me->SetImmuneToPC(true);
+
+            ScheduleHealthCheckEvent(30, [&]{
+                //handle for set phase 1
+                Talk(SAY_PHASE3);
+                scheduler.DelayAll(18s);
+                scheduler.Schedule(8s, [this](TaskContext /*context*/)
+                {
+                    DoCastSelf(SPELL_CAMERA_SHAKE, true);
+                    instance->SetData(DATA_COLLAPSE, GO_STATE_ACTIVE);
+                }).Schedule(15s, [this](TaskContext /*context*/)
+                {
+                    DoCastSelf(SPELL_COLLAPSE_DAMAGE, true);
+                    me->resetAttackTimer();
+                    //handle for set phase 0
+                    scheduler.Schedule(20s, [this](TaskContext context)
+                    {
+                        if (Unit* target = SelectTarget(SelectTargetMethod::Random))
+                        {
+                            target->CastSpell(target, SPELL_DEBRIS_VISUAL, true, nullptr, nullptr, me->GetGUID());
+                            me->m_Events.AddEvent(new DealDebrisDamage(*me, target->GetGUID()), me->m_Events.CalculateTime(5000));
+                        }
+                        context.Repeat(20s);
+                    })
+                });
+            });
         }
 
         void KilledUnit(Unit*  /*victim*/) override
@@ -138,10 +158,48 @@ public:
         {
             events2.Reset();
             _JustEngagedWith();
-            events.ScheduleEvent(EVENT_EMOTE1, 0);
-            events.ScheduleEvent(EVENT_EMOTE2, 60000);
-            events.ScheduleEvent(EVENT_EMOTE3, 120000);
-            events.ScheduleEvent(EVENT_ENTER_COMBAT, 123000);
+
+            Talk(SAY_EMOTE_BEGIN);
+
+            scheduler.Schedule(60s, [this](TaskContext /*context*/)
+            {
+                Talk(SAY_EMOTE_NEARLY);
+            }).Schedule(120s, [this](TaskContext /*context*/)
+            {
+                Talk(SAY_EMOTE_FREE);
+            }).Schedule(123s, [this](TaskContext /*context*/)
+            {
+                me->RemoveUnitFlag(UNIT_FLAG_NOT_SELECTABLE);
+                me->SetImmuneToPC(false);
+                me->SetReactState(REACT_AGGRESSIVE);
+
+                scheduler.Schedule(9s, [this](TaskContext context)
+                {
+                    DoCastVictim(SPELL_CLEAVE);
+                    context.Repeat(10s);
+                }).Schedule(10s, [this](TaskContext context)
+                {
+                    me->CastCustomSpell(SPELL_BLAZE, SPELLVALUE_MAX_TARGETS, 1);
+                    context.Repeat(30s);
+                }).Schedule(40s, [this](TaskContext context)
+                {
+                    me->CastSpell(me, SPELL_QUAKE); //needs fixes with custom spell
+                    scheduler.Schedule(7s, [this](TaskContext /*context*/)
+                    {
+                        DoCastSelf(SPELL_BLAST_NOVA);
+                        //leave with events until I find a better way to do it
+                        events.ScheduleEvent(EVENT_CANCEL_GRASP_CHECK, 12000);
+                        events2.ScheduleEvent(EVENT_CHECK_GRASP, 0);
+                    });
+                    context.Repeat(50s);
+                }).Schedule(1320s, [this](TaskContext /*context*/)
+                {
+                    DoCastSelf(SPELL_BERSERK, true);
+                });
+
+                instance->SetData(DATA_ACTIVATE_CUBES, 1);
+                me->RemoveAurasDueToSpell(SPELL_SHADOW_CAGE);
+            });
         }
 
         void UpdateAI(uint32 diff) override
@@ -149,10 +207,6 @@ public:
             events2.Update(diff);
             switch (events2.ExecuteEvent())
             {
-                case EVENT_RANDOM_TAUNT:
-                    Talk(SAY_TAUNT);
-                    events2.ScheduleEvent(EVENT_RANDOM_TAUNT, 90000);
-                    break;
                 case EVENT_CHECK_GRASP:
                     if (me->GetAuraCount(SPELL_SHADOW_GRASP_VISUAL) == 5)
                     {
@@ -173,85 +227,11 @@ public:
 
             switch (events.ExecuteEvent())
             {
-                case EVENT_EMOTE1:
-                    Talk(SAY_EMOTE_BEGIN);
-                    break;
-                case EVENT_EMOTE2:
-                    Talk(SAY_EMOTE_NEARLY);
-                    break;
-                case EVENT_EMOTE3:
-                    Talk(SAY_EMOTE_FREE);
-                    Talk(SAY_FREE);
-                    break;
-                case EVENT_ENTER_COMBAT:
-                    me->RemoveUnitFlag(UNIT_FLAG_NOT_SELECTABLE);
-                    me->SetImmuneToPC(false);
-                    me->SetReactState(REACT_AGGRESSIVE);
-                    events.ScheduleEvent(EVENT_CLEAVE, 9000);
-                    events.ScheduleEvent(EVENT_BLAZE, 10000);
-                    events.ScheduleEvent(EVENT_QUAKE, 40000);
-                    events.ScheduleEvent(EVENT_CHECK_HEALTH, 500);
-                    events.ScheduleEvent(EVENT_ENRAGE, 22 * MINUTE * IN_MILLISECONDS);
-
-                    instance->SetData(DATA_ACTIVATE_CUBES, 1);
-                    me->RemoveAurasDueToSpell(SPELL_SHADOW_CAGE);
-                    break;
-                case EVENT_CLEAVE:
-                    me->CastSpell(me->GetVictim(), SPELL_CLEAVE, false);
-                    events.ScheduleEvent(EVENT_CLEAVE, 10000);
-                    break;
-                case EVENT_BLAST_NOVA:
-                    me->CastSpell(me, SPELL_BLAST_NOVA, false);
-                    events.ScheduleEvent(EVENT_CANCEL_GRASP_CHECK, 12000);
-                    events2.ScheduleEvent(EVENT_CHECK_GRASP, 0);
-                    break;
-                case EVENT_BLAZE:
-                    me->CastCustomSpell(SPELL_BLAZE, SPELLVALUE_MAX_TARGETS, 1);
-                    events.ScheduleEvent(EVENT_BLAZE, 30000);
-                    break;
-                case EVENT_ENRAGE:
-                    me->CastSpell(me, SPELL_BERSERK, true);
-                    break;
                 case EVENT_CANCEL_GRASP_CHECK:
                     events2.Reset();
                     break;
-                case EVENT_QUAKE:
-                    me->CastSpell(me, SPELL_QUAKE, false);
-                    events.ScheduleEvent(EVENT_BLAST_NOVA, 7000);
-                    events.ScheduleEvent(EVENT_QUAKE, 50000);
-                    break;
-                case EVENT_CHECK_HEALTH:
-                    if (me->HealthBelowPct(30))
-                    {
-                        Talk(SAY_PHASE3);
-                        events.SetPhase(1);
-                        events.DelayEvents(18000);
-                        events.ScheduleEvent(EVENT_COLLAPSE_CEIL, 8000);
-                        events.ScheduleEvent(EVENT_COLLAPSE_DAMAGE, 15000);
-                        break;
-                    }
-                    events.ScheduleEvent(EVENT_CHECK_HEALTH, 500);
-                    break;
-                case EVENT_COLLAPSE_CEIL:
-                    me->CastSpell(me, SPELL_CAMERA_SHAKE, true);
-                    instance->SetData(DATA_COLLAPSE, GO_STATE_ACTIVE);
-                    break;
-                case EVENT_COLLAPSE_DAMAGE:
-                    me->CastSpell(me, SPELL_COLLAPSE_DAMAGE, true);
-                    me->resetAttackTimer();
-                    events.SetPhase(0);
-                    events.ScheduleEvent(EVENT_DEBRIS, 20000);
-                    break;
-                case EVENT_DEBRIS:
-                    if (Unit* target = SelectTarget(SelectTargetMethod::Random))
-                    {
-                        target->CastSpell(target, SPELL_DEBRIS_VISUAL, true, nullptr, nullptr, me->GetGUID());
-                        me->m_Events.AddEvent(new DealDebrisDamage(*me, target->GetGUID()), me->m_Events.CalculateTime(5000));
-                    }
-                    events.ScheduleEvent(EVENT_DEBRIS, 20000);
-                    break;
             }
-
+            //change to scheduler for this
             if (!events.IsInPhase(1))
                 DoMeleeAttackIfReady();
         }
