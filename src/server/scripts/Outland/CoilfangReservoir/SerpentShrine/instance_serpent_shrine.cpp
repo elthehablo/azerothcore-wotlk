776bb9aee369ee48a511f4bcb68da3d5b86f1cd7
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-08-18 15:27:17 +0200

    add lady vashj creaturedata

--- src/server/scripts/Outland/CoilfangReservoir/SerpentShrine/instance_serpent_shrine.cpp
+++ src/server/scripts/Outland/CoilfangReservoir/SerpentShrine/instance_serpent_shrine.cpp
@@ -32,6 +32,7 @@ DoorData const doorData[] =
 ObjectData const creatureData[] =
 {
     { NPC_LEOTHERAS_THE_BLIND, DATA_LEOTHERAS_THE_BLIND },
+    { NPC_LADY_VASHJ, DATA_LADY_VASHJ }
 };
 
 class instance_serpent_shrine : public InstanceMapScript
