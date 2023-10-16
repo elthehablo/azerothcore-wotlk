5e1166007c7c58fe5b9ff58fe43203f021df69b9
elthehablo <83884799+elthehablo@users.noreply.github.com>
2023-10-16 18:25:20 +0200

    remove debugging

--- src/server/scripts/Outland/CoilfangReservoir/SerpentShrine/boss_fathomlord_karathress.cpp
+++ src/server/scripts/Outland/CoilfangReservoir/SerpentShrine/boss_fathomlord_karathress.cpp
@@ -94,9 +94,11 @@ struct boss_fathomlord_karathress : public BossAI
         BossAI::Reset();
         _recentlySpoken = false;
 
-        me->SummonCreature(NPC_FATHOM_GUARD_TIDALVESS, advisorsPosition[0], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
-        me->SummonCreature(NPC_FATHOM_GUARD_SHARKKIS, advisorsPosition[1], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
-        me->SummonCreature(NPC_FATHOM_GUARD_CARIBDIS, advisorsPosition[2], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
+        //summons.DespawnAll();
+
+        //me->SummonCreature(NPC_FATHOM_GUARD_TIDALVESS, advisorsPosition[0], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
+        //me->SummonCreature(NPC_FATHOM_GUARD_SHARKKIS, advisorsPosition[1], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
+        //me->SummonCreature(NPC_FATHOM_GUARD_CARIBDIS, advisorsPosition[2], TEMPSUMMON_CORPSE_TIMED_DESPAWN, 600000);
 
         ScheduleHealthCheckEvent(75, [&]{
             for (SummonList::const_iterator itr = summons.begin(); itr != summons.end(); ++itr)
@@ -268,13 +270,8 @@ struct boss_fathomguard_sharkkis : public ScriptedAI
 
     void JustDied(Unit* /*killer*/) override
     {
-        if (_instance)
-        {
-            me->Yell("I have an instancescript", LANG_UNIVERSAL);
-        }
         if (Creature* karathress = _instance->GetCreature(DATA_FATHOM_LORD_KARATHRESS))
         {
-            me->Yell("found my master", LANG_UNIVERSAL);
             me->CastSpell(karathress, SPELL_POWER_OF_SHARKKIS, true);
         }
     }
