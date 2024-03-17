8e354c3494c48d9f31e6a86db597c58390713f72
elthehablo <83884799+elthehablo@users.noreply.github.com>
2024-03-17 12:07:07 +0100

    init

---
+++ data/sql/updates/pending_db_world/dagger.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry` = 36478;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(36478, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0);
